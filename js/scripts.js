7111bea65922c8ca984a40f1bdd109ce880706e0
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-26 11:13:45 -0700

    BUGFIX: add working function to get first result

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ $(document).ready(function () {
     event.preventDefault();
     const orange = $("input:radio[value=orange]:checked").val();
     const cat = $("input:radio[value=cat]:checked").val();
+    const pop = $("input:radio[value=pop]:checked").val();
+    const rain = $("input:radio[value=rain]:checked").val();
+    const hot = $("input:radio[value=hotcoffee]:checked").val();
+
+    if (cat,orange,pop,rain,hot) {
+      $("#result1").show()
+    } else {
+      $("#result2").show()
+    }
+  });
+}); 
